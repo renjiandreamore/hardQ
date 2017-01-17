1ad865afdef239a5497d84a901dc7703e7468dbe
jian ren <renjiandreamore@users.noreply.github.com>
2017-01-17 15:31:59 -0500

    Update N-Sum-Classes.java

--- N-Sum-Classes.java
+++ N-Sum-Classes.java
@@ -135,4 +135,56 @@ public class Solution {
     }
     
     
+    
+    
+    /**
+     * @param numbers : Give an array numbersbers of n integer
+     * @param target : you need to find four elements that's sum of target
+     * @return : Find all unique quadruplets in the array which gives the sum of
+     *           zero.
+     */
+    public ArrayList<ArrayList<Integer>> fourSum(int[] numbers, int target) {
+        /* your code */
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if(numbers == null || numbers.length == 0) return res;
+        Arrays.sort(numbers);
+        
+        for(int i = 0; i < numbers.length - 3; i++) {
+            if(i != 0 && numbers[i] == numbers[i-1]) continue;
+            for(int j = i + 1; j < numbers.length - 2; j++) {
+                if(j != i + 1 && numbers[j] == numbers[j-1]) continue;
+                int left = j + 1;
+                int right = numbers.length - 1;
+                while(left < right) {
+                    int sum = numbers[i] + numbers[j] + numbers[left] + numbers[right];
+                    if(sum < target){
+                        left++;
+                    }
+                    else if(sum > target) {
+                        right--;
+                    }
+                    else{ //sum == target
+                        ArrayList<Integer> path = new ArrayList<>();
+                        path.add(numbers[i]);
+                        path.add(numbers[j]);
+                        path.add(numbers[left]);
+                        path.add(numbers[right]);
+                        res.add(path);
+                        left++;
+                        right--;
+                        while(left < right && numbers[left] == numbers[left-1]){
+                            left++;
+                        }
+                        while(left < right && numbers[right] == numbers[right+1]){
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    //4 sum 问题还可以优化至n*n*logN, sort一下， 然后n*n两两pair， 然后等于是求一个two sum
 }
