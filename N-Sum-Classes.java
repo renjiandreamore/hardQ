ce2cd6d7bcfe32c8faaf620c8eadae73f16f8643
jian ren <renjiandreamore@users.noreply.github.com>
2017-01-17 15:07:00 -0500

    Update N-Sum-Classes.java

--- N-Sum-Classes.java
+++ N-Sum-Classes.java
@@ -100,5 +100,39 @@ public class Solution {
     } 
     
     
+    /**
+     * @param numbers: Give an array numbers of n integer
+     * @param target : An integer
+     * @return : return the sum of the three integers, the sum closest target.
+     */
+    public int threeSumClosest(int[] numbers ,int target) {
+        // write your code here
+        if(numbers == null || numbers.length == 0) return 0;
+        
+        Arrays.sort(numbers);
+        int ans = Integer.MAX_VALUE;
+
+        for(int i = 0; i < numbers.length; i++) {
+            int left = i + 1;
+            int right = numbers.length - 1;
+            while(left < right) {
+                int sum = numbers[i] + numbers[left] + numbers[right];
+                if(Math.abs(sum - target) < Math.abs(ans - target)) {
+                    ans = sum;
+                }
+                if(sum < target) {
+                    left ++;
+                }
+                else if(sum > target){
+                    right --;
+                }
+                else{
+                    return target;
+                }
+            }
+        }
+        return ans;
+    }
+    
     
 }
