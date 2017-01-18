26d202e114c36fc04b2e0991772351a79aa30e24
jian ren <renjiandreamore@users.noreply.github.com>
2017-01-18 10:01:32 -0500

    Update 子数组.java

--- 子数组.java
+++ 子数组.java
@@ -204,4 +204,36 @@ public class Solution {
         if(min == Integer.MAX_VALUE) return -1;
         return min;
     }
+    
+    /**
+     * @param nums: an array of integers
+     * @return: an integer
+     乘积最大子序列
+     */
+    public int maxProduct(int[] nums) {
+        // write your code here
+        if(nums == null || nums.length == 0) return 0;
+        
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+        max[0] = min[0] = nums[0];
+        int res = nums[0];
+        
+        for(int i = 1; i < nums.length; i++) {
+            max[i] = min[i] = nums[i];
+            if(nums[i] > 0) {
+                max[i] = Math.max(max[i], max[i-1] * nums[i]);
+                min[i] = Math.min(min[i-1] * nums[i], min[i]);
+            }
+            else if(nums[i] < 0) {
+                max[i] = Math.max(max[i], min[i-1] * nums[i]);
+                min[i] = Math.min(max[i-1] * nums[i], min[i]);
+            }
+            res = Math.max(res, max[i]);
+        }
+        
+        return res;
+    }
+    
+    
 }
