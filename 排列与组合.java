a13b340c99a970d5a36299c5e3d11af8e79b0947
jian ren <renjiandreamore@users.noreply.github.com>
2017-01-21 19:58:27 -0500

    Update 排列与组合.java

--- 排列与组合.java
+++ 排列与组合.java
@@ -31,4 +31,188 @@ public class Solution {
         
         return res + 1;
     }
+    
+    
+    
+    
+    /**  上一个排列
+     * @param nums: A list of integers
+     * @return: A list of integers that's previous permuation
+     */
+    public ArrayList<Integer> previousPermuation(ArrayList<Integer> nums) {
+		// write your code
+		
+		if(nums == null || nums.size() == 0) return new ArrayList<Integer>();
+		
+		int pos = nums.size();
+		for(int i = nums.size() - 1; i>= 1; i--) {
+		    if(nums.get(i) < nums.get(i - 1)) {
+		        pos = i - 1;
+		        break;
+		    }
+		}
+		
+		if(pos == nums.size()) {
+		    reverse(nums, 0, nums.size() - 1);
+		    return nums;
+		}
+		
+		int fst_small = pos + 1;
+		for(int i = nums.size() - 1; i>=pos+1; i--) {
+		    if(nums.get(i) < nums.get(pos)) {
+		        fst_small = i;
+		        break;
+		    }
+		}
+		
+		int tmp = nums.get(pos);
+		nums.set(pos, nums.get(fst_small));
+		nums.set(fst_small, tmp);
+		
+		reverse(nums, pos + 1, nums.size() - 1);
+		return nums;
+    }
+    
+    public void reverse(ArrayList<Integer> nums, int start, int end) {
+        for(int i = start, j = end; i < j; i++, j--) {
+            int tmp = nums.get(i);
+            nums.set(i, nums.get(j));
+            nums.set(j, tmp);
+        }
+    }
+    
+    
+    
+    /** 下一个排列
+     * @param nums: an array of integers
+     * @return: return nothing (void), do not return anything, modify nums in-place instead
+     */
+ public void nextPermutation(int[] nums) {
+        // write your code here
+        if(nums == null || nums.length == 0){
+            return;
+        }
+        if(nums.length == 1) return;
+        //1,2,4,3 --> 1, 3, 2, 4
+        
+        //find the last increse
+        //1,2,4,3 ---- 2
+        int index = nums.length; 
+        for(int i = nums.length - 2; i>=0 ; i--){
+            if(nums[i+1] > nums[i] ){
+                index = i;
+                break;
+            }
+        }
+        
+        if(index == nums.length){
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+        
+        //find the fisrt > nums[index] after index;
+        int right = index + 1;
+        for(int i = nums.length - 1; i>= right; i--){
+            if(nums[i] > nums[index]){
+                right = i;
+                break;
+            }
+        }
+        
+        int tmp = nums[index];
+        nums[index] = nums[right];
+        nums[right] = tmp;
+        
+        reverse(nums, index + 1, nums.length - 1);
+    }
+    
+    public void reverse(int[] nums, int left, int right){
+        for(int i = left, j = right; i < j; i++, j--){
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+    
+    
+    
+    
+    /**  全排列
+     * @param nums: A list of integers.
+     * @return: A list of permutations.
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        // write your code here
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0){
+          res.add(new ArrayList<Integer>());
+          return res;
+        };
+        
+        List<Integer> path = new ArrayList<Integer>();
+        
+        helper(nums, path, res);
+        return res;
+    }
+    
+    public void helper(int[] nums, List<Integer> path, List<List<Integer>> res) {
+        if(path.size() == nums.length) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(path.contains(nums[i])){
+                continue;
+            }
+            path.add(nums[i]);
+            helper(nums, path, res);
+            path.remove(path.size() - 1);
+        }
+    }
+    
+    
+    
+     
+    /**  带重复元素的全排列
+     * @param nums: A list of integers.
+     * @return: A list of unique permutations.
+     */
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        // Write your code here
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0) {
+            res.add(new ArrayList<Integer>());
+            return res;
+        }
+        
+        boolean[] visited = new boolean[nums.length];
+        Arrays.sort(nums);
+        
+        List<Integer> path = new ArrayList<>();
+        helper(nums, res, path, visited);
+        return res;
+    } 
+    
+    public void helper(int[] nums, List<List<Integer>> res, List<Integer> path, boolean[] visited) {
+        if(path.size() == nums.length) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(visited[i] || i != 0  && nums[i] == nums[i-1]&& !visited[i-1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            visited[i] = true;
+            helper(nums, res, path, visited);
+            visited[i] = false;
+            path.remove(path.size()-1);
+        }
+    }
+    
+    
+    
+    
 }
