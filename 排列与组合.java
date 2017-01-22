72f7a0c65da210a068730d5a8c4f80663dafe8c9
jian ren <renjiandreamore@users.noreply.github.com>
2017-01-22 13:46:21 -0500

    Update 排列与组合.java

--- 排列与组合.java
+++ 排列与组合.java
@@ -282,5 +282,79 @@ class Combinations{
          }
      }
    
+	
+	
+	
+	
+	//数字组合1
+      public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // write your code here
+        List<List<Integer>> res = new ArrayList<>();
+        if(candidates == null || candidates.length == 0) return res;
+        Arrays.sort(candidates);
+        
+        //boolean[] visited = new boolean[candidates.length];
+        List<Integer> path = new ArrayList<>();
+        helper(candidates, target, path, res, 0);
+        
+        return res;
+    }
+    
+    public void helper(int[] candidates, int target, List<Integer> path, List<List<Integer>> res, int index) {
+        //if(target < 0) return;
+        
+        if(target == 0) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        
+        for(int i = index; i < candidates.length; i++) {
+            if(target < candidates[i]) break;
+            if(i != 0 && candidates[i] == candidates[i-1]) continue;
+            
+            path.add(candidates[i]);
+            helper(candidates, target - candidates[i], path, res, i);
+            path.remove(path.size() - 1);
+        }
+    }
+	
+	
+	
+	//数字组合2	
+	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        // write your code here
+        List<List<Integer>> res = new ArrayList<>();
+        if(candidates == null || candidates.length == 0) return res;
+        Arrays.sort(candidates);
+        
+        boolean[] visited = new boolean[candidates.length];
+        List<Integer> path = new ArrayList<>();
+        helper(candidates, target, path, res, 0, visited);
+        
+        return res;
+    }
+    
+    public void helper(int[] candidates, int target, List<Integer> path, List<List<Integer>> res, int index, boolean[] visited) {
+        //if(target < 0) return;
+        
+        if(target == 0) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        
+        for(int i = index; i < candidates.length; i++) {
+            if(target < candidates[i]){
+                break;
+            }
+            if(visited[i] || i != 0 && !visited[i-1] && candidates[i] == candidates[i-1]){
+                continue;
+            }
+            path.add(candidates[i]);
+            visited[i] = true;
+            helper(candidates, target - candidates[i], path, res, i, visited);
+            visited[i] = false;
+            path.remove(path.size() - 1);
+        }
+    }
 
 }
