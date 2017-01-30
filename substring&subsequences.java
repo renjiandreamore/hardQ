f0498ed5703c597f5458f4819308998c9962dbbf
jian ren <renjiandreamore@users.noreply.github.com>
2017-01-30 18:20:38 -0500

    Update substring&subsequences.java

--- substring&subsequences.java
+++ substring&subsequences.java
@@ -66,6 +66,54 @@ public class Solution {
     }
   
   
-  
+  /**
+     * @param A, B: Two strings.
+     * @return: The length of longest common subsequence of A and B.
+     给出"ABCD" 和 "EDCA"，这个LCS是 "A" (或 D或C)，返回1
+
+        给出 "ABCD" 和 "EACB"，这个LCS是"AC"返回 2
+     */
+    
+    //最长公共子序列
+    public int longestCommonSubsequence(String A, String B) {
+        // write your code here
+        if(A == null || B == null || A.length() == 0 || B.length() == 0) return 0;
+        
+        int m = A.length(), n = B.length();
+        int[][] dp = new int[m][n];
+        
+        for(int i = 0; i < n; i++) {
+            if(A.charAt(0) == B.charAt(i)){
+                dp[0][i] = 1;
+                for(int j = i + 1; j < n; j++) {
+                    dp[0][j] = 1;
+                }
+                break;
+            }
+        }
+        
+        for(int i = 0; i < m; i++) {
+            if(B.charAt(0) == A.charAt(i)) {
+                dp[i][0] = 1;;
+                for(int j = i + 1; j < m; j++) {
+                    dp[j][0] = 1;
+                }
+                break;
+            }
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(A.charAt(i) == B.charAt(j)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
   
 }
