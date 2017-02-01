4a07f2fe2e6c461cd350825b93ca1f93d4d549ec
jian ren <renjiandreamore@users.noreply.github.com>
2017-02-01 10:46:01 -0500

    Update substring&subsequences.java

--- substring&subsequences.java
+++ substring&subsequences.java
@@ -34,6 +34,34 @@ public class SubSequences {
         
         return max;
     }
+    
+    
+    //distinct subsequences
+    public int numDistinct(String S, String T) {
+        // write your code here
+        if(S == null || S.length() == 0 || T == null) return 0;
+        int m = S.length(), n = T.length();
+        int[][] dp = new int[m+1][n+1];
+        
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i >= j) {
+                    dp[i][j] = dp[i-1][j];
+                    if(S.charAt(i-1) == T.charAt(j-1)) {
+                        dp[i][j] += dp[i-1][j-1];
+                    }
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+    
+    
   
   
   /**
