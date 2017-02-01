478af5ae356dd9fff832d3f09a12542a742268de
jian ren <renjiandreamore@users.noreply.github.com>
2017-02-01 11:53:09 -0500

    Update substring&subsequences.java

--- substring&subsequences.java
+++ substring&subsequences.java
@@ -261,4 +261,47 @@ public class SubStrings {
         return source.substring(start_point, start_point + min_len);
     }
     
+    //最长回文子串
+    /**
+     * @param s input string
+     * @return the longest palindromic substring
+     */
+     
+     /**
+      * abcba这样的奇数个长度可由c左右推，看是否相等判断。但是abba这样偶数长度，我们没法找到b和b之间的那个数，所以思想是无论奇偶我们都把长度扩大一倍，扩大后的偶数各位用#表示，扩大后的奇数各位用原字符串表示.
+      * 
+        abba --> #a#b#b#a ,后面再补一个#即可, 有效性是从第三个#开始左右移动判断。
+        abcba --> #a#b#c#b#a, 后面再补一个#即可，有效性是从c开始左右移动判断
+      */
+    public String longestPalindrome(String s) {
+        // Write your code here
+        if(s == null || s.length() == 0 || s.length() == 1) return s;
+        
+        String res = "";
+        int sum = 0;
+        
+        for(int i = 1; i < 2*s.length(); i++) {
+            int count = 1;
+            while(i - count >= 0 && i + count <= 2*s.length() && get(s, i-count) == get(s, i+count)) {
+                count++;
+            }
+            count--;
+            
+            if(count > sum) {
+                sum = count;
+                res = s.substring((i - count)/2, (i + count)/2);
+            }
+        }
+        return res;
+    }
+    
+    public char get(String s, int index) {
+        if(index % 2 == 0){
+            return '_';
+        }
+        else{
+            return s.charAt(index/2);
+        }
+    }
+    
 }
