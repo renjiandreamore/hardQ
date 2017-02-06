8214a0afb092eade44512d46d81fa8423ae007f9
jian ren <renjiandreamore@users.noreply.github.com>
2017-02-06 16:08:21 -0500

    Update 股票系列.java

--- 股票系列.java
+++ 股票系列.java
@@ -22,6 +22,18 @@ public class BestTime2BuyAndSellStock {
         return max;
     }
     
-    
-    
+    //可以买多次
+    public int maxProfit2(int[] prices) {
+        // write your code here
+        if(prices == null || prices.length == 0) return 0;
+        int profit = 0;
+        
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] - prices[i-1] > 0) {
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        
+        return profit;
+    }
 }
