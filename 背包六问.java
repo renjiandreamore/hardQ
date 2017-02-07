89054fbd176fdf45de47cb34c25e5cb60c95bc76
jian ren <renjiandreamore@users.noreply.github.com>
2017-02-07 10:59:46 -0500

    Update 背包六问.java

--- 背包六问.java
+++ 背包六问.java
@@ -28,4 +28,26 @@ public class Solution {
         
         return dp[n][m];
     }
+    
+    
+    //单次选择+最大价值
+    public int backPackII(int m, int[] A, int V[]) {
+        // write your code here
+        if(m < 0 || A.length == 0 || V.length == 0) return 0;
+        int n = V.length;
+        
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(j >= A[i-1]) {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - A[i-1]] + V[i-1]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
 }
